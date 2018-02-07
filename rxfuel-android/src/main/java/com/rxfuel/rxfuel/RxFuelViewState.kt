87ee8cd932d1a167e514a73e15368d176a68ae57
Salah <nh.salah@gmail.com>
2018-02-08 03:15:48 +0530

    Version 0.0.3

--- rxfuel-android/src/main/java/com/rxfuel/rxfuel/RxFuelViewState.kt
+++ rxfuel-android/src/main/java/com/rxfuel/rxfuel/RxFuelViewState.kt
@@ -5,12 +5,15 @@ import kotlin.reflect.KClass
 
 /**
  * Interface to implement ViewState. ViewState ares used to render the UI.
- * Implementation should override property [navigate]. Set it null by default.
- * Change the value of [navigate] to destination activity class to handle navigation.
- * A new ViewState will be trigger with [navigate] = null immediately after rendering the ViewState with navigation.
  *
  * @author Salah (nh.salah@gmail.com)
  */
 interface RxFuelViewState {
+
+    /**
+     * Implementation should override property [navigate]. Set it null by default.
+     * Change the value of [navigate] to destination activity class to handle navigation.
+     * A new ViewState will be trigger with [navigate] = null immediately after rendering the ViewState with navigation.
+     */
     var navigate : KClass<out FragmentActivity>?
 }
