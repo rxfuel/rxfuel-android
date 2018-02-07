b3a8880feb6b8809ba4a090fc148be65e8af64b9
Salah <nh.salah@gmail.com>
2018-02-08 03:22:09 +0530

    Doc update

--- rxfuel-android/src/main/java/com/rxfuel/rxfuel/RxFuelEvent.kt
+++ rxfuel-android/src/main/java/com/rxfuel/rxfuel/RxFuelEvent.kt
@@ -1,13 +1,18 @@
 package com.rxfuel.rxfuel
 
 /**
- * Interface to implement Event. Implementation should override property [isLocal].
- * Set [isLocal] to true if the event doesn't require async processing.
- * Process and map local events directly to result in [RxFuelViewModel.eventToResult]
- * If [isLocal] is set to false, create corresponding action and handle process in processor class.
+ * Interface to implement Event.
+ * Events should hold data of all user interactions and Android provided data such as location updates, gyro etc
  *
  * @author Salah (nh.salah@gmail.com)
  */
 interface RxFuelEvent{
+
+    /**
+     * Implementation should override property [isLocal].
+     * Set [isLocal] to true if the event doesn't require async processing.
+     * Process and map local events directly to result in [RxFuelViewModel.eventToResult]
+     * If [isLocal] is set to false, create corresponding action and handle process in processor class.
+     */
     val isLocal : Boolean
 }
